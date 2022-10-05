d2bae15d6a467ddacd5121a6bdc9ae6184f805d9
ggcldev <ggcldev@gmail.com>
2022-10-06 00:30:43 +0800

    Add js for the mobile menu

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,17 @@
+const btn = document.getElementById('menu-btn')
+const menu = document.getElementById('menu')
+const logo = document.getElementById('logo')
 
+btn.addEventListener('click', navToggle)
+
+function navToggle() {
+	btn.classList.toggle('open')
+	menu.classList.toggle('flex')
+	menu.classList.toggle('hidden')
+
+	// if (menu.classList.contains('flex')) {
+	// 	logo.setAttribute('src', '../dist/images/')
+	// } else {
+	// 	logo.setAttribute('src', '../dist/images/')
+	// }
+}
