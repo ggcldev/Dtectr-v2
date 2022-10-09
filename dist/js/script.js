c44dcd45dc7ce0316eeecd065b362c571fa485de
ggcldev <ggcldev@gmail.com>
2022-10-10 07:12:34 +0800

    Added two png for product section - Added product section

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,6 +2,9 @@ const btn = document.getElementById('menu-btn')
 const menu = document.getElementById('menu')
 const logo = document.getElementById('logo')
 
+const tabs = document.querySelectorAll('.tab')
+const panels = document.querySelectorAll('.panel')
+
 btn.addEventListener('click', navToggle)
 
 function navToggle() {
@@ -14,4 +17,29 @@ function navToggle() {
 	// } else {
 	// 	logo.setAttribute('src', '../dist/images/')
 	// }
+}
+
+// Tabs menu event listener
+tabs.forEach((tab) => tab.addEventListener('click', onTabClick))
+
+function onTabClick(e) {
+	// Deactivate all tabs
+	tabs.forEach((tab) => {
+		tab.children[0].classList.remove(
+			'border-secondaryColor',
+			'border-b-4',
+			'md:border-b-0'
+		)
+	})
+
+	// Hide all panels
+	panels.forEach((panel) => panel.classList.add('hidden'))
+
+	// Activate a new tab and panel based on the target
+	e.target.classList.add('border-secondaryColor', 'border-b-4')
+	const classString = e.target.getAttribute('data-target')
+	document
+		.getElementById('panels')
+		.getElementsByClassName(classString)[0]
+		.classList.remove('hidden')
 }
